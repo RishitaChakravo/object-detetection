5c262ef098f157c6143f16bdd37594a05a134db2
Rishita Chakravorty <chakravorty.r20@outlook.com>
2026-02-28 11:57:12 +0530

    carcounter done, added tracker which helps in counting

--- Project1_Car Counter/carcounter.py
+++ Project1_Car Counter/carcounter.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
+from sort import *
 
 # cap = cv2.VideoCapture(0)
 # cap.set(3, 640)
@@ -25,6 +26,12 @@ classNames = [
 model = YOLO('../Yolo-Weights/yolov8n.pt')
 mask = cv2.imread("mask.png")
 mask = cv2.resize(mask, (1280, 720))
+limits = [400, 415, 1200, 415] 
+totalcnts = []
+
+# Tracking
+tracker = Sort(max_age=20, min_hits=2, iou_threshold=0.3)
+
 while True:
     success, img = cap.read()
     img = cv2.resize(img, (1280, 720))
@@ -33,6 +40,9 @@ while True:
     imgRegion = cv2.bitwise_and(img, mask)
     
     results = model(imgRegion, stream=True)
+
+    detections = np.empty((0,5))
+
     for r in results:
         boxes = r.boxes
         for box in boxes:
@@ -46,9 +56,30 @@ while True:
             classname = classNames[cls]
 
             if classname == "car" or classname == "truck" or classname == "bus" or classname == "motorbike" and conf > 0.3:  
-                cvzone.cornerRect(img, bbox, l=5)  
+                # cvzone.cornerRect(img, bbox, l=5, rt=5)  
                 cvzone.putTextRect(img, f'{classname} {conf}', (max(0,x1), max(0,y1-20)), scale=0.6, thickness=1)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
 
+    result_Tracker = tracker.update(detections)
+    cv2.line(img,(limits[0], limits[1]),(limits[2], limits[3]), (0, 0, 255), 5)
+
+    for result in result_Tracker:
+        x1, y1,x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2-x1, y2-y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=5, rt=2, colorR=(255,0,255))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0,x1), max(0,y1-20)), scale=2, thickness=10, offset=10)
+
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img,(cx, cy),5,(255, 0, 255), cv2.FILLED)
+
+        if limits[0]< cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if totalcnts.count(id) == 0:
+                totalcnts.append(id)
+                cv2.line(img,(limits[0], limits[1]),(limits[2], limits[3]), (0, 255, 0), 5)
+
+    cvzone.putTextRect(img, f'{int(len(totalcnts))}', (50,50))
     cv2.imshow("Image", img)
-    cv2.imshow("ImageRegion", imgRegion)
+    # cv2.imshow("ImageRegion", imgRegion)
     cv2.waitKey(1)
